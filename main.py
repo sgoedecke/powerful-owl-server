d7940cc892747b2beb5126e54f4eac4425049970
Sean Goedecke <sgoedecke@github.com>
2024-02-27 15:03:12 +1100

    hoot

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ model_name = "sgoedecke/wav2vec2_owl_classifier_v3"
 # classifier = pipeline("audio-classification", model=model_name)
 
 hf_client = InferenceClient(model=model_name)
-classifier = hf_client.audio_classification()
+classifier = hf_client.audio_classification
 
 def convert_audio_to_wav(audio_bytes):
     # Load audio file from bytes and convert to WAV with 16kHz
