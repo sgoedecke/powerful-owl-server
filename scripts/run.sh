0e76a4833e9389ffc161b3f042f11af147b032c9
Sean Goedecke <sgoedecke@github.com>
2024-03-03 08:24:02 +1100

    hoot

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 pkill gunicorn
 git pull
 source /root/owl-server/bin/activate
-gunicorn main:app --bind 0.0.0.0:443 --certfile /etc/letsencrypt/live/ninoxstrenua.site/fullchain.pem --keyfile /etc/letsencrypt/live/ninoxstrenua.site/privkey.pem --daemon
+gunicorn main:app -w 2 --bind 0.0.0.0:443 --certfile /etc/letsencrypt/live/ninoxstrenua.site/fullchain.pem --keyfile /etc/letsencrypt/live/ninoxstrenua.site/privkey.pem --daemon
 
 # Certificate is saved at: /etc/letsencrypt/live/ninoxstrenua.site/fullchain.pem
 # Key is saved at:         /etc/letsencrypt/live/ninoxstrenua.site/privkey.pem
--- scripts/run_local.sh
+++ scripts/run_local.sh
@@ -1,3 +1,3 @@
 pkill gunicorn
 git pull
-gunicorn main:app --bind 0.0.0.0:443 --certfile /etc/letsencrypt/live/ninoxstrenua.site/fullchain.pem --keyfile /etc/letsencrypt/live/ninoxstrenua.site/privkey.pem
+gunicorn main:app -w 2 --bind 0.0.0.0:443 --certfile /etc/letsencrypt/live/ninoxstrenua.site/fullchain.pem --keyfile /etc/letsencrypt/live/ninoxstrenua.site/privkey.pem
