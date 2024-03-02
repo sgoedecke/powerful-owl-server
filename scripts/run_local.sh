2b4756504e6363fd1a93dd019b98373b01e74c7f
Sean Goedecke <sgoedecke@github.com>
2024-03-03 08:35:12 +1100

    hoot

--- scripts/run_local.sh
+++ scripts/run_local.sh
@@ -1,3 +1,3 @@
 pkill gunicorn
 git pull
-gunicorn main:app -w 2 --bind 0.0.0.0:443 --certfile /etc/letsencrypt/live/ninoxstrenua.site/fullchain.pem --keyfile /etc/letsencrypt/live/ninoxstrenua.site/privkey.pem
+gunicorn main:app -w 2 --timeout 30000 --bind 0.0.0.0:443 --certfile /etc/letsencrypt/live/ninoxstrenua.site/fullchain.pem --keyfile /etc/letsencrypt/live/ninoxstrenua.site/privkey.pem
